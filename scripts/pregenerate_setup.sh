719461f425a5f49bef61565d98ec95842cfe2afa
Sebastian Delgado <netmanzion@gmail.com>
2018-09-21 13:35:38 -0300

    Change test.html new version

---
+++ scripts/pregenerate_setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# This is the folder where we copy the code
+CODE_VERSION="/var/www/codeversion"
+
+# We keep a history of the releases on this folder
+RELEASE_DIR="/var/www/code_release"
+
+# Project folder name
+PROJECT_FOLDER="/var/www/html"
+
+# See this for a list of possible values:
+# https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+#TIMEZONE=America/New_York
+TIMEZONE="UTC"
+
+#######################################
+###### NOT EDIT BEYOND THIS LINE ######
+#######################################
+
+# Exporting paths
+export PATH=$PATH:/bin:/usr/bin:/usr/local/bin
+
+# Get THIS_RELEASE folder name
+THIS_RELEASE=$(TZ=$TIMEZONE date +%Y%m%d%H%M%S)
+
+# Create release folder name
+mkdir -p "${RELEASE_DIR}/${THIS_RELEASE}"
+
+# Copying Version
+rsync -av ${CODE_VERSION}/  ${RELEASE_DIR}/${THIS_RELEASE}
+
+# Get Latest version
+VERSION=$(ls ${RELEASE_DIR}/ | tail -n 1)
+
+# Generate Symlink to latest version
+rm -rf ${PROJECT_FOLDER}; ln -s ${RELEASE_DIR}/${VERSION} ${PROJECT_FOLDER}
